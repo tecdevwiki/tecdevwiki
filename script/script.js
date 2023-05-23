7f0ff752f2514274d6b85b9ee7f7188397fc7e31
tecdevwiki <134443274+tecdevwiki@users.noreply.github.com>
2023-05-23 16:41:06 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,17 @@
+/* <script>
+        function clickMenu() {
+            if (items.style.display == 'block') {
+                items.style.display = 'none'
+            } else {
+                items.style.display = 'block' 
+            }
+        }
+    </script> */
+
+function clickMenu() {
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
+}
